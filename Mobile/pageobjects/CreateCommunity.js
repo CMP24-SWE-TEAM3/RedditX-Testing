1ef6a186375c490dd4adc231c6eae0f60d3b4552
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-12-15 13:42:48 +0200

    add the object selectors in profile and createcommunity

---
+++ Mobile/pageobjects/CreateCommunity.js
@@ -0,0 +1,38 @@
+const {
+    returnPOM
+} = require('../utils/utils');
+module.exports = class CreateCommunity {
+
+    async getCommunityNameField() {
+        const selector = await '';
+        return returnPOM(selector);
+    }
+    async getCommunitytypeButton() {
+        const selector = await '';
+        return returnPOM(selector);
+    }
+    async getCommunitytypeButton() {
+        const selector = await '';
+        return returnPOM(selector);
+    }
+    async getPublicButton() {
+        const selector = await '';
+        return returnPOM(selector);
+    }
+    async getPrivateButton() {
+        const selector = await '';
+        return returnPOM(selector);
+    }
+    async getRestrictedButton() {
+        const selector = await '';
+        return returnPOM(selector);
+    }
+    async getplus18CommunityCheckBox() {
+        const selector = await '';
+        return returnPOM(selector);
+    }
+    async getCreateCommunityButton() {
+        const selector = await '';
+        return returnPOM(selector);
+    }
+}
--- Mobile/pageobjects/Profile.js
+++ Mobile/pageobjects/Profile.js
@@ -3,40 +3,261 @@ const {
     returnPOMs,
     ScreenContains
 } = require('../utils/utils');
-module.exports = class Profile {
-    //  Profile Posts
-    async PostBlogName() {
-        const selector = await 'new UiSelector().resourceId("com.cmp.cmplr:id/username_home")';
-        return returnPOMs(selector);
+module.exports = class Profile{
+    async getPostsTab(){
+        const selector= await('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
     }
-    // async PostContent() {
-    //     const selector = await 'new UiSelector().resourceId("com.tumblr:id/dashboard_blocks_text")';
-    //     return returnPOMs(selector);
-    // }
-    // async PostTags() {
-    //     const selector = await 'new UiSelector().resourceId("com.tumblr:id/post_card_wrapped_tags")';
-    //     return returnPOMs(selector);
-    // }
-    async PostLikeButton() {
-        const selector = await 'new UiSelector().resourceId("com.cmp.cmplr:id/love_btn")';
-        return returnPOMs(selector);
-    }
-    async PostCommentButton() {
-        const selector = await 'new UiSelector().resourceId("com.cmp.cmplr:id/comments_btn")';
-        return returnPOMs(selector);
-    }
-    // async PostReblogButton() {
-    //     const selector = await 'new UiSelector().resourceId("com.tumblr:id/post_control_reblog")';
-    //     return returnPOMs(selector);
-    // }
-    // async PostDeleteButton() {
-    //     const selector = await 'new UiSelector().resourceId("com.tumblr:id/post_control_delete")';
-    //     return returnPOMs(selector);
-    // }
-    // async PostEditButton() {
-    //     const selector = await 'new UiSelector().resourceId("com.tumblr:id/post_control_edit")';
-    //     return returnPOMs(selector);
-    // }
 
+    async getCommentsTab(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getAboutTab(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getLikeButton(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getNumberOfLikes(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getAgetDislikeButtonboutTab(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getNumberOfLikes(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getCommentButton(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getInputField(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getCommentArea(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getPostCommentButton(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getCommentWithText(des){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getAboutPost(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getAboutPostDropdown(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getAboutPost(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+
+    async getdropdownMarkSpolier(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+
+    async getdropdownMarkNSFW(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getdropdownLockComments(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getdropdownsticky(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getdropdownRemovePost(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getdropdownMarkSpam(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getdropdownApprove(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getdropdownMarkSpolier(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getShareButton(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getShareDropdown(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getInviteButton(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getShareProfile(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getSocialLinkButton(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getInstagramButton(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getShagetUsernameFieldOfSocialLinkreProfile(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getContinueButtonFromSocialLink(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+
+
+    async getFollowersButton(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getSearchFieldOfFollowers(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getResultsButton(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getResultsWindowOfFollowes(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getEditProfileButton(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getAreaOfAddNameToProfile(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getContentVisbilitycheckBox(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getAreaOfDescriptonToProfile(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getSaveButtonFromEditProfile(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getShareProfile(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getShareProfile(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    async getShareProfile(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+    async getShareProfile(){
+        const selector=await ('new UiSelector().className("android.widget.EditText").instance(0).index(0)');
+        return returnPOM(selector);
+    }
+
+
+    
 }
-
