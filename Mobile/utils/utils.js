b845a6eb51d353a6b458ba2b0d4c43185c580c52
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-12-24 18:26:50 +0200

    final utils

--- Mobile/utils/utils.js
+++ Mobile/utils/utils.js
@@ -29,3 +29,4 @@ module.exports.ScreenContains = async (text, time = 3000) => {
     return POM;
 }
 
+
--- Mobile/utils/utilsLogin.js
+++ Mobile/utils/utilsLogin.js
@@ -14,3 +14,15 @@ module.exports.chooseInterests=async()=>{
         const threeButton=await LoginPOM.getInterestsButtonThree();
         await threeButton.click();
 }
+
+
+module.exports.login=async()=>{
+    const contineuButton=await  LoginPOM.getContineuWithEmail();
+    await contineuButton.click();
+    const usernameField=await LoginPOM.getUsernameField();
+    await usernameField.addValue("mahmoud_reda");
+    const passwordField=await LoginPOM.getPasswordField();
+    await passwordField.addValue("123456789");
+    const loginButton =await LoginPOM.getLoginButton();
+    await loginButton.click();
+}
