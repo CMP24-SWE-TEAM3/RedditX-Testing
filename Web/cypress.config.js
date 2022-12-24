69730e500c335cd42c14c82c547c80c3827feaba
mahmoud abdelhamid <mahmoud13abdelhamid@gmail.com>
2022-12-24 18:54:20 +0200

    update Config file

--- Web/cypress.config.js
+++ Web/cypress.config.js
@@ -1,6 +1,14 @@
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+    reporterOptions: {
+       reportDir: "cypress/results",
+       overwrite: false,
+       html: true,
+       json: true
+    },
   projectId: '54nb39',
   viewportWidth: 1650,
   viewportHeight: 1033,
